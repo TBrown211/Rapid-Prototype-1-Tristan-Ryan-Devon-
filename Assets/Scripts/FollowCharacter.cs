24a92f24c6de91057010208c5935589aadedf2e2
COdinRyT <150468038+COdinRyT@users.noreply.github.com>
2024-09-24 18:15:25 -0400

    Changed C# Script
    With the help of some AI scripting, I can easily adjust the angle of the jetpack object and simulate following the player

--- Assets/Scripts/FollowCharacter.cs
+++ Assets/Scripts/FollowCharacter.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
-{
-    public Transform player;    
-    private Vector3 offset = new Vector3(0, 0.73f, -0.759f);
-
+{    
+    public Transform player;    //Creating a reference to keep track of players position
+    private Vector3 offset = new Vector3(0, 1f, -0.759f);
+    private Vector3 objectRot = new Vector3(15.09f, 0, 0);
     
     
 
@@ -18,9 +18,11 @@ public class NewBehaviourScript : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {       
+    {        
         transform.position = player.position + player.rotation * offset;
 
-        transform.rotation = player.rotation;     
+        Quaternion angle = Quaternion.Euler(objectRot);
+        transform.rotation = player.rotation * angle; 
+
     }
 }
