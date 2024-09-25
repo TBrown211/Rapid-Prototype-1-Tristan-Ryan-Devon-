8778d8e9f00d18baa80ed1ca28153ec322b199b8
COdinRyT <150468038+COdinRyT@users.noreply.github.com>
2024-09-25 13:25:36 -0400

    Add C# Script
    added a C# script for the main flying mechanic

---
+++ Assets/Scripts/JetpackFlight.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetpackFlight : MonoBehaviour
+{
+    private float flightForce; //the amount of force that the jetpack will propel to
+    private float flightSpeed;
+    public float flightTime; //how much time that the player will fly for
+    private bool flying = false; //a boolean to keep track when player is flying
+    private bool grounded = true; //a boolean to keep track of when player is on ground
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
